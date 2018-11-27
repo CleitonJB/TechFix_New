6cca7f8d683265bbc4aca7386a36a1f86bfe90ee
CleitonJB <cleitonbraga56@gmail.com>
2018-11-27 10:02:56 -0200

    Alterando os códigos relacionados à postagem de comentário.

---
+++ NetBeans/src/main/webapp/forum.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TechFix - Home</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">  
+  <link href="css/flexslider.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="LogoMarca/22520889_1442280212_1920.ico" type="image/x-icon">
+    <link href="css/Forum.css" rel="stylesheet">
+  </head>
+  <body>
+  	<!-- Header -->
+  	<div class="tm-header">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
+  					<a href="Index.html" class="tm-site-name">TECHFIX</a>	
+  				</div>
+	  			<div class="col-lg-6 col-md-8 col-sm-9">
+	  				<div class="mobile-menu-icon">
+		              <i class="fa fa-bars"></i>
+		            </div>
+	  				<nav class="tm-nav" style="width: 700px">
+						<ul>
+							<li><a href="Index.html" class="active">Menu</a></li>
+							<li><a href="Sobre.html">Sobre</a></li>
+							<li><a href="Empresas.html">Empresas</a></li>
+							<li><a href="Login.php">Login</a></li>
+							<li><a href="Cadastro.php">Cadastro</a></li>
+
+
+
+						</ul>
+					</nav>		
+	  			</div>				
+  			</div>
+  		</div>	  	
+  	</div>
+	<!-- <section class="container tm-home-section-1" id="more" style="position: relative; top: 100px;">
+    <div class="col-lg-4 col-md-4 col-sm-6">
+        
+        <div class="tm-home-box-1">
+          <ul class="nav nav-tabs tm-white-bg" role="tablist" id="hotelCarTabs">
+              <li role="presentation" class="active">
+                <a href="#hotel" aria-controls="hotel" role="tab" data-toggle="tab">Pesquisar por Categoria</a>
+              </li>
+              <li role="presentation">
+                <a href="#car" aria-controls="car" role="tab" data-toggle="tab">Pesquisar por empresas</a>
+              </li>
+          </ul>
+
+          <div class="tab-content">
+              <div role="tabpanel" class="tab-pane fade in active tm-white-bg" id="hotel">
+                <div class="tm-search-box effect2">
+                <form action="#" method="post" class="hotel-search-form">
+                  <div class="tm-form-inner">
+                    <div class="form-group">
+                             <select class="form-control">
+                              <option value="">-- Selecione a Categoria -- </option>
+                              <option value="shangrila">Computadores</option>
+                        <option value="chatrium">Celulares</option>
+                        <option value="fourseasons">Notebook</option>
+                        <option value="hilton">Outros</option>
+                      </select> 
+                          </div>
+                          <div class="form-group">
+                              <div>
+                                  <input type='text' class="form-control" placeholder="Empresas" />
+                                  <span>
+                                      <span></span>
+                                  </span>
+                              </div>
+                          </div>
+                            <div class="form-group margin-bottom-0">
+                                <select class="form-control">
+                              <option value="estado">--Estados-- </option>
+                        <option value="ac">Acre</option> 
+                        <option value="al">Alagoas</option> 
+                        <option value="am">Amazonas</option> 
+                        <option value="ap">Amapá</option> 
+                        <option value="ba">Bahia</option> 
+                        <option value="ce">Ceará</option> 
+                        <option value="df">Distrito Federal</option> 
+                        <option value="es">Espírito Santo</option> 
+                        <option value="go">Goiás</option> 
+                        <option value="ma">Maranhão</option> 
+                        <option value="mt">Mato Grosso</option> 
+                        <option value="ms">Mato Grosso do Sul</option> 
+                        <option value="mg">Minas Gerais</option> 
+                        <option value="pa">Pará</option> 
+                        <option value="pb">Paraíba</option> 
+                        <option value="pr">Paraná</option> 
+                        <option value="pe">Pernambuco</option> 
+                        <option value="pi">Piauí</option> 
+                        <option value="rj">Rio de Janeiro</option> 
+                        <option value="rn">Rio Grande do Norte</option> 
+                        <option value="ro">Rondônia</option> 
+                        <option value="rs">Rio Grande do Sul</option> 
+                        <option value="rr">Roraima</option> 
+                        <option value="sc">Santa Catarina</option> 
+                        <option value="se">Sergipe</option> 
+                        <option value="sp">São Paulo</option> 
+                        <option value="to">Tocantins</option> 
+                      </select> 
+                          </div>
+                  </div>              
+                        <div class="form-group tm-yellow-gradient-bg text-center">
+                          <button type="submit" name="submit" class="tm-yellow-btn">Procurar</button>
+                        </div>  
+                </form>
+              </div>
+              </div>
+              <div role="tabpanel" class="tab-pane fade tm-white-bg" id="car">
+              <div class="tm-search-box effect2">
+                <form action="#" method="post" class="hotel-search-form">
+                  <div class="tm-form-inner">
+                    <div class="form-group">
+                             <select class="form-control">        
+                        <option value="">-- Selecione a Categoria -- </option>
+                              <option value="shangrila">Computadores</option>
+                        <option value="chatrium">Celulares</option>
+                        <option value="fourseasons">Notebook</option>
+                        <option value="hilton">Outros</option>
+                      </select> 
+                          </div>
+                          <div class="form-group">
+                              <div class='input-group date-time' id='datetimepicker3'>
+                                  <input type='text' class="form-control" placeholder="Pickup Date" />
+                                  <span class="input-group-addon">
+                                      <span class="fa fa-calendar"></span>
+                                  </span>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <div class='input-group date-time' id='datetimepicker4'>
+                                  <input type='text' class="form-control" placeholder="Return Date" />
+                                  <span class="input-group-addon">
+                                      <span class="fa fa-calendar"></span>
+                                  </span>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                             <select class="form-control">
+                              <option value="">-- Opções -- </option>
+                              <option value=""> Software </option>
+                        <option value=""> Hardware </option>
+                        <option value=""> Outros </option>
+                      </select> 
+                          </div>                         
+                  </div>              
+                        <div class="form-group tm-yellow-gradient-bg text-center">
+                          <button type="submit" name="submit" class="tm-yellow-btn">Procurar</button>
+                        </div>  
+                </form>
+              </div>
+              </div>            
+          </div>
+        </div>                
+      </div>
+  </section> -->
+
+          <aside style="position: relative; float: right; top: 100px; left: -50px"> 
+            <dl>
+              <dt>Empresas</dt>
+                <dd onclick="window.open('Index.html')">Acre</dd>
+                <dd onclick="window.open('Index.html')">Alagoas</dd>
+                <dd onclick="window.open('Index.html')">Amapá</dd>
+                <dd onclick="window.open('Index.html')">Amazonas</dd>
+                <dd onclick="window.open('Index.html')">Bahia</dd>
+                <dd onclick="window.open('Index.html')">Ceará</dd>
+                <dd onclick="window.open('Index.html')">Distrito Federal</dd>
+                <dd onclick="window.open('Index.html')">Espírito Santo</dd>
+                <dd onclick="window.open('Index.html')">Goiás</dd>
+                <dd onclick="window.open('Index.html')">Maranhão</dd>
+                <dd onclick="window.open('Index.html')">Mato Grosso</dd>
+                <dd onclick="window.open('Index.html')">Mato Grosso do Sul</dd>
+                <dd onclick="window.open('Index.html')">Minas Gerais</dd>
+                <dd onclick="window.open('Index.html')">Pará</dd>
+                <dd onclick="window.open('Index.html')">Paraíba</dd>
+                <dd onclick="window.open('Index.html')">Paraná</dd>
+                <dd onclick="window.open('Index.html')">Pernambuco</dd>
+                <dd onclick="window.open('Index.html')">Piauí</dd>
+                <dd onclick="window.open('Index.html')">Rio de Janeiro</dd>
+                <dd onclick="window.open('Index.html')">Rio Grande do Norte</dd>
+                <dd onclick="window.open('Index.html')">Rio Grande do Sul</dd>
+                <dd onclick="window.open('Index.html')">Rondônia</dd>
+                <dd onclick="window.open('Index.html')">Roraima</dd>
+                <dd onclick="window.open('Index.html')">Santa Catarina</dd>
+                <dd onclick="window.open('Index.html')">São Paulo</dd>
+                <dd onclick="window.open('Index.html')">Sergipe</dd>
+                <dd onclick="window.open('Index.html')">Tocantins</dd>
+            </dl>
+          </aside>
+
+          <div style="margin-top: 50px;"> 
+
+            <div class="Bloquinho" align="center">
+              <div class="Tituluzinho">
+                <div style="font-size: 18px; position: relative; top: 2px">Comente</div>
+              </div>
+                <textarea name="comentário" rows="10" cols="40"></textarea>
+            </dl>
+            </div>
+
+            <dl class="Topicos">
+              <dd>Reclamação</dd>
+                <dt>Pousou em meio às rochas. Suas asas se fecharam, seu corpo começava a se esfriar. O peso dos anos fazia suas penas se esfarelarem e sua chama amiudar. Percebeu que era a hora da partida. Olhou para o céu estrelado, morrer era uma das leis da natureza, mas nem por isso era algo bom. </dt>
+            </dl>
+
+            <dl class="Topicos">
+              <dd>Solicitação</dd>
+                <dt>Guinchou fracamente, seu fogo apagou, seu corpo caiu no chão e se transformou em cinzas negras.
+Após alguns segundos, as partículas das cinzas começaram a se agitar a ponto de queimarem. Juntaram-se em um redemoinho de fogo para formar um novo ser. Das chamas surgiu uma nova ave a piar sons melancólicos.</dt>
+            </dl>
+
+            <dl class="Topicos">
+              <dd>Informação</dd>
+                <dt>Sonhos, planos, objetivos feitos em delírios conjugais. Pensamentos que a realidade fez questão de mostrar que estavam errados. Tudo perfeitamente planejado, mas que não deixou de ser apenas um plano que não teve a oportunidade de sair do papel, pior, não teve  nem a oportunidade de sair das mentes juvenis apaixonadas para ir ao papel.</dt>
+            </dl>
+
+            <dl class="Topicos">
+              <dd>Solicitação</dd>
+                <dt>É comum eu ouvir de algumas mães que não vêem a hora do filho crescer e contar pra ela sobre a percepção de ouvir cada som com o IC, tal como eu faço. Ainda que...</dt>
+            </dl>
+            <dl class="Topicos">
+              <dd>Debate</dd>
+                <dt>Pousou em meio às rochas. Suas asas se fecharam, seu corpo começava a se esfriar. O peso dos anos fazia suas penas se esfarelarem e sua chama amiudar. Percebeu que era a hora da partida. Olhou para o céu estrelado, morrer era uma das leis da natureza, mas nem por isso era algo bom. </dt>
+            </dl>
+          </div>
+      </body>
+  </html>
