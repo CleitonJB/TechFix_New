9bb757bb23ac0daf1dc02a6df1de31ce8b7362ad
CleitonJB <cleitonbraga56@gmail.com>
2018-12-04 10:51:59 -0200

    Sistema de comentários. 1/2

--- NetBeans/src/main/webapp/index.php
+++ NetBeans/src/main/webapp/index.php
@@ -1,44 +1,43 @@
-<!-- https://inatitude.wordpress.com/2010/12/06/tutorial-php-sistema-de-comentarios/ -->
-
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″ />
-<title>Sistema de comentário</title>
-</head>
-<body>
-<h1>Digite seu comentário</h1>
-<form method=”post” action=”validar.php”>
-<div>Nome: </div>
-<div><input type=”text” name=”nome” />
-</div>
-<div>Email: </div>
-<div>
-<input type=”text” name=”email” />
-</div>
-<div>Comentário: </div>
-<div>
-<textarea cols=”30″ rows=”5″ name=”comentario”></textarea>
-</div>
-<hr />
-<div>
-<input type=”submit” value=”Enviar” />
-</div>
-</form>
+	<head>
+		<meta charset="utf-8">
+		<title>Sistema de comentário</title>
+	</head>
+	<body>
+		<!-- https://inatitude.wordpress.com/2010/12/06/tutorial-php-sistema-de-comentarios/ -->
+		<h1>Digite seu comentário - Funciona criatura divina</h1>
 
-<?php
+		<form method="post" action="validar.php">
+			<div>Nome: </div>
+				<div>
+					<input type="text" name="nome" />
+				</div>
+			<div>Email: </div>
+				<div>
+					<input type="text" name="email" />
+				</div>
+			<div>Comentário: </div>
+				<div>
+					<textarea cols="30" rows="5" name="comentario"></textarea>
+				</div>
+			<hr/>
+			<div>
+				<input type="submit" value="Enviar" />
+			</div>
+		</form>
 
-	require(‘conexao.php’);
+		<?php
+		
+			require('conexao.php');
 
-	$tabela = mysql_query(“SELECT * FROM comentarios ORDER BY id DESC”);
-
-	while($exibir = mysql_fetch_array($tabela)){
-		echo $exibir[‘nome’] . “<br />”;
-		echo $exibir[‘email’] . “<br />”;
-		echo $exibir[‘comentario’] . “<hr />”;
-	}
-
-?>
-
-</body>
+			$tabela = mysql_query("SELECT * FROM comentarios ORDER BY id DESC");
+			$exibir = mysql_fetch_array($tabela);
+			while($exibir = mysql_fetch_array($tabela)){
+				echo $exibir['nome'];
+				echo $exibir['email'];
+				echo $exibir['comentario'];
+			}
+		?>
+	</body>
 </html>
--- NetBeans/src/main/webapp/validar.php
+++ NetBeans/src/main/webapp/validar.php
@@ -1,18 +1,29 @@
-<<?php 
-	require(‘conexao.php’);
+<html>
+<head><title>Sistema de comentário =D</title>
+<!-- <script>alert("será?")</script> -->
+</head>
+<body>
+teoricamente foi... <br>
+<?php 
 
-	$nome = $_POST[‘nome’];
-	$email = $_POST[‘email’];
-	$comentario = $_POST[‘comentario’];
+	require('conexao.php');
+
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$comentario = $_POST['comentario'];
+
 
 	if(empty($nome)){
-		echo “<script>alert(‘Atenção! O campo nome não pode ficar em branco. )</script>”;
-		echo ‘<script>history.back();</script>’;
+		echo '<script>alert("Atenção! O campo nome não pode ficar vazio.")</script>';
 	} else if(empty($email)){
-		echo “<script>alert(‘Atenção! O campo email não pode ficar em branco. )</script>”;
-		echo ‘<script>history.back();</script>’;
+		echo '<script>';
+		echo 'alert("Atenção! O campo email não pode ficar vazio.")';
+		echo '</script>';
 	} else if(empty($comentario)){
-		echo “<script>alert(‘Atenção! O campo comentario não pode ficar em branco. )</script>”;
-		echo ‘<script>history.back();</script>’;
-	} 
+		echo '<script>';
+		echo 'alert("Atenção! O campo comentario não pode ficar vazio.")';
+		echo '</script>';
+	}
 ?>
+</body>
+</html>
