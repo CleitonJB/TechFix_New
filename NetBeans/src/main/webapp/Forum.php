55fcbc6349295bd0177ba64fbf2e21ededb48ef9
CleitonJB <cleitonbraga56@gmail.com>
2018-12-17 17:45:27 -0200

    Adicionando modificações visuais em Forum.php e FOrum.css

--- NetBeans/src/main/webapp/Forum.php
+++ NetBeans/src/main/webapp/Forum.php
@@ -1,5 +1,3 @@
-<!-- https://www.oficinadanet.com.br/artigo/php/sistema_de_comentarios_php_-_mysql -->
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -30,63 +28,65 @@
 	<body>
 
 	  	<!-- Header -->
-	  	<div class="tm-header">
-	  		<div class="container">
-	  			<div class="row">
-	  				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
-	  					<a href="Index.html" class="tm-site-name">TECHFIX</a>	
-	  				</div>
-		  			<div class="col-lg-6 col-md-8 col-sm-9">
-		  				<div class="mobile-menu-icon">
-			              <i class="fa fa-bars"></i>
-			            </div>
-		  				<nav class="tm-nav" style="width: 700px">
-							<ul>
-								<li><a href="Index.html" class="active">Menu</a></li>
-								<li><a href="Sobre.html">Sobre</a></li>
-								<li><a href="Empresas.html">Empresas</a></li>
-								<li><a href="Login.php">Login</a></li>
-								<li><a href="Cadastro.php">Cadastro</a></li>
-							</ul>
-						</nav>		
-		  			</div>				
-	  			</div>
-	  		</div>	  	
-	  	</div>
+  	<div class="tm-header">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
+  					<img style="position: relative; bottom: 15px;" onclick="location='Index.php'" class="LogoMarca" src="LogoMarca/LogoMarca.png" title="Seja Bem-vindo(a) ao TechFix!"> 
+  				</div>
+	  			<div class="col-lg-6 col-md-8 col-sm-9">
+	  				<div class="mobile-menu-icon">
+		              <i class="fa fa-bars"></i>
+		            </div>
+	  				<nav class="tm-nav" style="width: 700px">
+						<ul>
+							<li><a href="Index.php" class="active">Menu</a></li>
+							<li><a href="Sobre.html">Sobre</a></li>
+							<li><a href="Empresas.html">Empresas</a></li>
+							<li><a href="Login.php">Login</a></li>
+							<li><a href="Cadastro.php">Cadastro</a></li>
+						</ul>
+					</nav>		
+	  			</div>				
+  			</div>
+  		</div>	  	
+  	</div>
 
 	    <!-- Menu lateral com as empresas por região -->
-	    <aside class="MenuLateral"> 
-	      <dl>
-	        <dt>Empresas</dt>
-	          <dd onclick="window.open('Index.html')">Acre</dd>
-	          <dd onclick="window.open('Index.html')">Alagoas</dd>
-	          <dd onclick="window.open('Index.html')">Amapá</dd>
-	          <dd onclick="window.open('Index.html')">Amazonas</dd>
-	          <dd onclick="window.open('Index.html')">Bahia</dd>
-	          <dd onclick="window.open('Index.html')">Ceará</dd>
-	          <dd onclick="window.open('Index.html')">Distrito Federal</dd>
-	          <dd onclick="window.open('Index.html')">Espírito Santo</dd>
-	          <dd onclick="window.open('Index.html')">Goiás</dd>
-	          <dd onclick="window.open('Index.html')">Maranhão</dd>
-	          <dd onclick="window.open('Index.html')">Mato Grosso</dd>
-	          <dd onclick="window.open('Index.html')">Mato Grosso do Sul</dd>
-	          <dd onclick="window.open('Index.html')">Minas Gerais</dd>
-	          <dd onclick="window.open('Index.html')">Pará</dd>
-	          <dd onclick="window.open('Index.html')">Paraíba</dd>
-	          <dd onclick="window.open('Index.html')">Paraná</dd>
-	          <dd onclick="window.open('Index.html')">Pernambuco</dd>
-	          <dd onclick="window.open('Index.html')">Piauí</dd>
-	          <dd onclick="window.open('Index.html')">Rio de Janeiro</dd>
-	          <dd onclick="window.open('Index.html')">Rio Grande do Norte</dd>
-	          <dd onclick="window.open('Index.html')">Rio Grande do Sul</dd>
-	          <dd onclick="window.open('Index.html')">Rondônia</dd>
-	          <dd onclick="window.open('Index.html')">Roraima</dd>
-	          <dd onclick="window.open('Index.html')">Santa Catarina</dd>
-	          <dd onclick="window.open('Index.html')">São Paulo</dd>
-	          <dd onclick="window.open('Index.html')">Sergipe</dd>
-	          <dd onclick="window.open('Index.html')">Tocantins</dd>
-	      </dl>
-	    </aside>
+	    <div class="MenuLateral">
+	    	<aside> 
+		      <dl>
+		      	<dt>Empresas</dt>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Acre</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Alagoas</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Amapá</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Amazonas</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Bahia</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Ceará</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Distrito Federal</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Espírito Santo</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Goiás</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Maranhão</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Mato Grosso</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Mato Grosso do Sul</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Minas Gerais</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Pará</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Paraíba</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Paraná</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Pernambuco</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Piauí</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Rio de Janeiro</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Rio Grande do Norte</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Rio Grande do Sul</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Rondônia</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Roraima</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Santa Catarina</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp São Paulo</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Sergipe</dd>
+		          <dd onclick="window.open('Index.php')">&nbsp &nbsp Tocantins</dd>
+		      </dl>
+		    </aside>
+	    </div>
 
     	<div class="CampoComentarios">
 			<form class="Formulario" method="post" action="validar.php">
@@ -135,9 +135,27 @@
 			if ($result->num_rows > 0) {
 			    // Mostrar todos comentários existentes no banco de dados
 			    while($row = $result->fetch_assoc()) {
-			    	// TENHO QUE ORGANIZAR ESSA BAGUNÇA PORQUE ESTÁ HORRÍVEL DE VER E MODIFICAR
-			        echo "<div class=\"Bloco\">" . "<div class=\"NomeEmail\">" . "<div class=\"Nome\">" . $row["nome"] . "</div>" . " - " . "<div class=\"Email\">" . $row["email"]. "</div>" . "</div>" . "<br>" . "<div class=\"Comentario\">" . "&nbsp" . "&nbsp" . "&nbsp" . $row["comentario"]. "</div>" . "<br>" . "<br>" . "</div>";
+			    	
 			        echo "<hr>";
+
+			        echo "<div class=\"Bloco\">";
+				        echo "<div class=\"NomeEmail\">";
+					        echo "<div class=\"Nome\">";
+					        	echo $row["nome"] . " - ";
+					        echo "</div>";
+
+					        echo "<div class=\"Email\">";
+					        	echo $row["email"];
+					        echo "</div>";
+					    echo "</div>";
+
+				        echo "</br>";
+
+				        echo "<div class=\"Comentario\">";
+					        echo "&nbsp" . "&nbsp" . "&nbsp"; 
+					        echo $row["comentario"]; 
+					    echo "</div>";
+				    echo "</div>";
 			    }
 			} else {
 			    echo "Nenhum comentário encontrado! :)";
@@ -148,11 +166,25 @@
 		<!-- CSSzinho dos comentários. Bagunçado, porém, funcional -->
 		<style type="text/css">
 			body{
-				background: #DFDFDE; /* Remover quando por no projeto principal. O mesmo já possuí um background definido */
+				background: #DFDFDE; /* Remover quando por no projeto principal. O mesmo já possuí um background definido #1CCA96 */
+			}
+
+			.Bloco {
+				border-radius: 15px;
+			    margin-top: 10px;
+			    margin-left: 170px;
+			    padding: 10px;
+			    background-color: black;
+			    border: 1px solid #FCDD44;
+			    width: 900px;
+			    height: 110px;
+			    color: #000000;
+			    box-shadow: 6px 4px 12px black;
+			    overflow-y: scroll;
 			}
 
 			.Nome{
-				color: #1CCA96;
+				color: #FCDD44;
 				margin-right: 10px;
 				text-shadow: 1px 2px 2px gray;
 			}
@@ -162,10 +194,16 @@
 				top: 2px;
 				font-size: 12px;
 				margin-left: 10px;
+				color: white;
+				text-decoration: none;
+			}
+
+			.Email:hover{
+				text-decoration: underline;
 			}
 
 			.NomeEmail{
-				color: white;
+				color: #FCDD44;
 				display: flex;
 				flex-direction: horizontal;
 			}
@@ -175,48 +213,6 @@
 				color: gray;
 				max-height: 90px;
 			}
-
-			.Bloco {
-				border-radius: 15px;
-			    margin-top: 10px;
-			    margin-left: 170px;
-			    padding: 10px;
-			    background-color: black;
-			    border: 1px solid #1CCA96;
-			    width: 900px;
-			    color: #000000;
-			    box-shadow: 6px 4px 12px black;
-			    overflow-y: scroll;
-			
-			}
 		</style>
-
-    	<!-- <div class="TodosComentarios"> 
-
-            <dl class="Topicos">
-              <dd>Reclamação</dd>
-                <dt>Pousou em meio às rochas. Suas asas se fecharam, seu corpo começava a se esfriar. O peso dos anos fazia suas penas se esfarelarem e sua chama amiudar. Percebeu que era a hora da partida. Olhou para o céu estrelado, morrer era uma das leis da natureza, mas nem por isso era algo bom. </dt>
-            </dl>
-
-            <dl class="Topicos">
-              <dd>Solicitação</dd>
-                <dt>Guinchou fracamente, seu fogo apagou, seu corpo caiu no chão e se transformou em cinzas negras.
-                Após alguns segundos, as partículas das cinzas começaram a se agitar a ponto de queimarem. Juntaram-se em um redemoinho de fogo para formar um novo ser. Das chamas surgiu uma nova ave a piar sons melancólicos.</dt>
-            </dl>
-
-            <dl class="Topicos">
-              <dd>Informação</dd>
-                <dt>Sonhos, planos, objetivos feitos em delírios conjugais. Pensamentos que a realidade fez questão de mostrar que estavam errados. Tudo perfeitamente planejado, mas que não deixou de ser apenas um plano que não teve a oportunidade de sair do papel, pior, não teve  nem a oportunidade de sair das mentes juvenis apaixonadas para ir ao papel.</dt>
-            </dl>
-
-            <dl class="Topicos">
-              <dd>Solicitação</dd>
-                <dt>É comum eu ouvir de algumas mães que não vêem a hora do filho crescer e contar pra ela sobre a percepção de ouvir cada som com o IC, tal como eu faço. Ainda que...</dt>
-            </dl>
-            <dl class="Topicos">
-              <dd>Debate</dd>
-                <dt>Pousou em meio às rochas. Suas asas se fecharam, seu corpo começava a se esfriar. O peso dos anos fazia suas penas se esfarelarem e sua chama amiudar. Percebeu que era a hora da partida. Olhou para o céu estrelado, morrer era uma das leis da natureza, mas nem por isso era algo bom. </dt>
-            </dl>
-    	</div> -->
       </body>
   </html>
