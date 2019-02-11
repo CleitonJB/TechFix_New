99ce26462998ed9a21d99856eb4ef814ede08440
CleitonJB <cleitonbraga56@gmail.com>
2019-02-11 10:43:07 -0200

    Atualizando o Forum (Fr.php e Fr.css)

---
+++ NetBeans/src/main/webapp/Fr.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+	<head>
+		<title> TechFix - Fórum </title>
+
+		<link rel="shortcut icon" href="LogoMarca/Icone.png" type="image/x-icon">
+		<link rel="stylesheet" type="text/css" href="Fr.css">
+
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+	  	<link href="css/bootstrap.min.css" rel="stylesheet">
+	  	<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">  
+	  	<link href="css/flexslider.css" rel="stylesheet">
+	  	<link href="css/templatemo-style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<!-- Header -->
+	  	<div class="tm-header">
+	  		<div class="container">
+	  			<div class="row">
+	  				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
+	  					<img onclick="window.open('Index.php', '_self');" class="LogoMarca" src="LogoMarca/LogoMarca.png" title="Seja Bem-vindo(a) ao TechFix!">
+	  				</div>
+		  			<div class="col-lg-6 col-md-8 col-sm-9">
+		  				<div class="mobile-menu-icon">
+			              <i class="fa fa-bars"></i>
+			            </div>
+		  				<nav class="tm-nav" style="width: 700px">
+							<ul>
+								<li><a href="Index.php" class="active">Menu</a></li>
+								<li><a href="Sobre.html">Sobre</a></li>
+								<li><a href="Empresas.html">Empresas</a></li>
+								<li><a href="Login.php">Login</a></li>
+								<li><a href="Cadastro.php">Cadastro</a></li>
+							</ul>
+						</nav>		
+		  			</div>				
+	  			</div>
+	  		</div>	  	
+	  	</div>
+
+	  	<!-- SECTION -->
+	  	<section>
+		  	<div class="TituloPosicao">				
+				<div class="AlinhaTitulo">
+					<hr>
+					<h2>Principais postagens</h2>
+					<hr>
+				</div>
+			</div>
+
+			<div class="FundoTopicos">
+				<div class="Tópico">
+					<h4> Computador não liga </h4> <!-- Título --> <br> <br>
+					<p align="justify">Assim como a fonte, qualquer problema na placa mãe o computador pode não ligar, ou as vezes a luz de power acende, mas nada acontece. Verifique se todos os cabos estão conectados corretamente, principalmente o da alimentação da placa mãe e o do processador. ... Vai ter que investir em outra placa-mãe.</p> <!-- Descrição -->
+				</div>
+
+				<div class="Tópico">
+					<h4> Computador não liga </h4> <!-- Título --> <br> <br>
+					<p align="justify">Assim como a fonte, qualquer problema na placa mãe o computador pode não ligar, ou as vezes a luz de power acende, mas nada acontece. Verifique se todos os cabos estão conectados corretamente, principalmente o da alimentação da placa mãe e o do processador. ... Vai ter que investir em outra placa-mãe.</p> <!-- Descrição -->
+				</div>
+
+				<div class="Tópico">
+					<h4> Computador não liga </h4> <!-- Título --> <br> <br>
+					<p align="justify">Assim como a fonte, qualquer problema na placa mãe o computador pode não ligar, ou as vezes a luz de power acende, mas nada acontece. Verifique se todos os cabos estão conectados corretamente, principalmente o da alimentação da placa mãe e o do processador. ... Vai ter que investir em outra placa-mãe.</p> <!-- Descrição -->
+				</div>
+			</div>
+		</section>
+
+		<!-- ASIDE -->
+		<aside>
+			<center>
+				<!-- MAIS ACESSADOS -->
+				<div class="Seccao">
+					<div class="row">				
+						<div class="AlinaTopico" class="tm-section-header"">
+							<hr>
+							<h6><b>Páginas mais acessadas</b></h6> 
+							<hr>
+						</div>
+					</div>
+					<div class="PrincipaisDestaques">
+						<center>
+							<button><a href=""> Principal tópico acessado 1 </a></button>
+							<button><a href=""> Principal tópico acessado 2 </a></button>
+							<button><a href=""> Principal tópico acessado 3 </a></button>
+							<button><a href=""> Principal tópico acessado 4 </a></button>
+							<button><a href=""> Principal tópico acessado 5 </a></button>
+							<button><a href=""> Principal tópico acessado 6 </a></button>
+							<button><a href=""> Principal tópico acessado 7 </a></button>
+							<button><a href=""> Principal tópico acessado 8 </a></button>
+							<button><a href=""> Principal tópico acessado 9 </a></button>
+							<button><a href=""> Principal tópico acessado 10 </a></button>
+						</center>
+					</div>
+				</div>	
+
+				<!-- EMPRESAS POR REGIÃO -->
+				<div class="Seccao">
+					<div>				
+						<div class="AlinaTopico" class="tm-section-header"">
+							<hr>
+							<h6><b>Empresas por região</b></h6> 
+							<hr>
+						</div>
+					</div>
+					<div class="PrincipaisDestaques">
+						<center>
+							<button><a href=""> Acre </a></button>
+							<button><a href=""> Alagoas </a></button>
+							<button><a href=""> Amapá </a></button>
+							<button><a href=""> Amazonas </a></button>
+							<button><a href=""> Bahia </a></button>
+							<button><a href=""> Ceará </a></button>
+							<button><a href=""> Distrito Federal </a></button>
+							<button><a href=""> Espírito Santo </a></button>
+							<button><a href=""> Goiás </a></button>
+							<button><a href=""> Maranhão </a></button>
+							<button><a href=""> Mato Grosso </a></button>
+							<button><a href=""> Mato Grosso do Sul </a></button>
+							<button><a href=""> Minas Gerais </a></button>
+							<button><a href=""> Pará </a></button>
+							<button><a href=""> Paraíba </a></button>
+							<button><a href=""> Paraná </a></button>
+							<button><a href=""> Pernambuco </a></button>
+							<button><a href=""> Piauí </a></button>
+							<button><a href=""> Rio de Janeiro </a></button>
+							<button><a href=""> Rio Grande do Norte </a></button>
+							<button><a href=""> Rio Grande do Sul </a></button>
+							<button><a href=""> Rondônia </a></button>
+							<button><a href=""> Roraima </a></button>
+							<button><a href=""> Santa Catarina </a></button>
+							<button><a href=""> São Paulo </a></button>
+							<button><a href=""> Sergipe </a></button>
+							<button><a href=""> Tocantins </a></button>
+						</center>
+					</div>
+				</div>
+			</center>
+		</aside>
+
+		<!-- ARTICLE -->
+		<center>
+			<article>
+				
+					<div class="Banner">
+						<p></p>
+					</div>
+				
+			</article>
+		</center>
+
+		<!-- FOOTER -->
+		<footer>
+			
+		</footer>
+	</body>
+</html>
